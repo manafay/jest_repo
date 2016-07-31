0c4d27f0ca4f75449e0a6ea2345a1f840c1dd419
Pervez Iqbal <pervezfunctor@gmail.com>
2016-07-31 23:26:19 +0530

    support flow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,27 @@
 module.exports = {
+  "parser": "babel-eslint",
   "extends": "airbnb",
   "installedESLint": true,
   "plugins": [
-    "react"
+    "react",
+    "flowtype",
   ],
   "rules": {
-    "no-console": "off"
+    "no-console": "off",
+    "flowtype/define-flow-type": 1,
+    "flowtype/space-after-type-colon": [
+      1,
+      "always"
+    ],
+    "flowtype/space-before-type-colon": [
+      1,
+      "never"
+    ],
+    "flowtype/use-flow-type": 1
+  },
+  "settings": {
+    "flowtype": {
+      "onlyFilesWithFlowAnnotation": false
+    }
   }
 };
