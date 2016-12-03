445f16b553f07d57cce3e3224227c11ab6d99005
Nafay <manafay@gmail.com>
2016-12-04 01:44:03 +0530

    Fix eslint issue

--- src/reducer.js
+++ src/reducer.js
@@ -5,8 +5,8 @@ function createReducer(initialState, actions) {
 
   const reducer = (state, action) => {
     globalState = state;
-    return Object.keys(allActions).filter(key => action.type === key.pop())
-                           .map(key => { key };);
+    Object.keys(allActions).filter(key => action.type === key.pop())
+                                  .map(key => key);
     return globalState;
   };
   return { reducer };
