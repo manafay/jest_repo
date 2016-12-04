355a20ffc65cd474c6fae88edf8a7204a079de6f
Nafay <manafay@gmail.com>
2016-12-04 08:33:00 +0530

    Code fix

--- src/reducer.js
+++ src/reducer.js
@@ -6,7 +6,7 @@ function createReducer(initialState, actions) {
   const reducer = (state, action) => {
     globalState = state;
     Object.keys(allActions).filter(key => action.type === key.pop())
-                                  .map(key => key);
+                                  .map(key => key(globalState));
     return globalState;
   };
   return { reducer };
