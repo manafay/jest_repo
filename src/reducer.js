c31adde80c71b0cd4e02342f5684a192d1c4c250
Nafay <manafay@gmail.com>
2016-12-11 08:25:04 +0530

    Modified createReducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,15 +1,40 @@
 
 function createReducer(initialState, actions) {
-  let globalState = typeof initialState !== 'undefined' ? initialState : [];
+  const globalState = typeof initialState !== 'undefined' ? initialState : [];
   const allActions = actions;
 
-  const reducer = (state, action) => {
-    globalState = state;
-    Object.keys(allActions).filter(key => action.type === key.pop())
-                                  .map(key => key(globalState));
-    return globalState;
+  const reducer = (state = globalState, action) => {
+    Object.keys(allActions).filter(key => action.type === key)
+                           .map(key => key(state, action));
+    return state;
   };
   return { reducer };
 }
 
 export default createReducer;
+
+// const r = createReducer({ count: 0 }, {
+//   INCREMENT: (state, action) => state.merge({
+//     count: state.count + 1,
+//     token: action.payload.token,
+//   }),
+//
+//   DECREMENT: (state, action) => state.merge({
+//     count: state.count - 1,
+//     token: action.payload.token,
+//   }),
+// });
+//
+// function createAction(actionType) {
+//   const action = {};
+//   if (actionType)
+//     action.type = actionType.toUpperCase();
+//   return action;
+// }
+//
+//
+// console.log(r);
+//
+// const INCREMENT = 'INCREMENT';
+// console.log(r.reducer({ count: 4 }, createAction(INCREMENT)));
+// console.log(r.check(INCREMENT));
