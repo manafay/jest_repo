26f87b7325c7a7028d2fe9646dfac121f33a48ac
Nafay <manafay@gmail.com>
2017-01-16 00:28:42 +0530

    Added createSelector and its corresponding test

--- src/reducer.js
+++ src/reducer.js
@@ -6,34 +6,9 @@ function createReducer(initialState, actions) {
   const reducer = (state = globalState, action) => {
     Object.keys(allActions).filter(key => action.type === key)
                            .map(key => allActions[key](state, action));
-    return state;
+    // return state;
   };
   return { reducer };
 }
 
 export default createReducer;
-
-// const r = createReducer({ count: 0 }, {
-//   INCREMENT: (state, action) => state.merge({
-//     count: state.count + 1,
-//     token: action.payload.token,
-//   }),
-//
-//   DECREMENT: (state, action) => state.merge({
-//     count: state.count - 1,
-//     token: action.payload.token,
-//   }),
-// });
-//
-// function createAction(actionType) {
-//   const action = {};
-//   if (actionType)
-//     action.type = actionType.toUpperCase();
-//   return action;
-// }
-//
-//
-// console.log(r);
-//
-// const INCREMENT = 'INCREMENT';
-// console.log(r.reducer({ count: 4 }, createAction(INCREMENT)));
